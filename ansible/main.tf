0cc324610cfabdc9ad88ab901260b7ef2da074ae
Tanner Filip <tannerfilip@gmail.com>
2015-10-13 07:59:30 -0500

    security group stuff

--- ansible/main.tf
+++ ansible/main.tf
@@ -4,6 +4,19 @@ provider "aws" {
   region = "us-east-1" # For now we only use east-1, so hardcoding this in
 }
 
+resource "aws_security_group" "ansible" {
+  name = "ansible"
+  description = "For Ansible use"
+  vpc_id = "vpc-ddf024b9"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/0","134.161.0.0/16"]
+  }
+}
+
 resource "aws_instance" "ansible" {
   ami = "ami-d05e75b8" # This is the Ubuntu 14.04 hvm image
   instance_type = "t2.micro"
@@ -11,6 +24,8 @@ resource "aws_instance" "ansible" {
     app = "ansible"
     env = "production"
   }
-  vpc_security_group_ids = ["${lookup(aws_security_group.ansible.id)}"]
+  vpc_security_group_ids = ["${aws_security_group.ansible.id}"]
   key_name = "tanner"
+  availability_zone = "us-east-1b"
+  subnet_id = "${lookup(var.production-subnets, aws_instance.ansible.availability_zone)}"
 }
--- ansible/variables.tf
+++ ansible/variables.tf
@@ -1,2 +1,15 @@
 variable "access_key" {}
 variable "secret_key" {}
+
+variable "production-subnets" {
+  defaults = {
+    us-east-1b-public = "subnet-d2174ff9"
+    us-east-1b-private = "subnet-6a3e6641"
+    us-east=1c-public = "subnet-a18899d6"
+    us-east=1c-private = "subnet-e3a0b194"
+    us-east-1d-public = "subnet-d1a78c88"
+    us-east-1d-private = "subnet-798fa420"
+    us-east-1e-public = "subnet-80e12cbd"
+    us-east-1e-private = "subnet-7608c54b"
+  }
+}
