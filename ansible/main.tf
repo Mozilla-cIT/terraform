386f1545126182c81d69ebf73352258d7889c218
Tanner Filip <tannerfilip@gmail.com>
2015-10-13 09:31:19 -0500

    fix typo

--- ansible/main.tf
+++ ansible/main.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "ansible" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["10.0.0.0/0","134.161.0.0/16"]
+    cidr_blocks = ["10.0.0.0/8","134.161.0.0/16"]
   }
 }
 
