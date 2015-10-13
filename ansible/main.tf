f36800bdedc253feb094f83a3d33467ded2783f3
Tanner Filip <tannerfilip@gmail.com>
2015-10-12 20:59:32 -0500

    create ansible server

---
+++ ansible/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1" # For now we only use east-1, so hardcoding this in
+}
+
+resource "aws_instance" "ansible" {
+  ami = "ami-d05e75b8" # This is the Ubuntu 14.04 hvm image
+  instance_type = "t2.micro"
+}
